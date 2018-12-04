2a40debe64d96d8f15ef8a575e832b885083133c
Morozov Alexander <webdevelopacc@gmail.com>
2018-12-04 15:49:10 +0700

    add database logic to wallet mapper

--- src/App/Model/Wallet.php
+++ src/App/Model/Wallet.php
@@ -13,4 +13,63 @@ class Wallet
      * @var integer
      */
     protected $moneyAmount;
+
+    /**
+     * @var Currency
+     */
+    protected $currency;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMoneyAmount(): int
+    {
+        return $this->moneyAmount;
+    }
+
+    /**
+     * @param int $moneyAmount
+     */
+    public function setMoneyAmount(int $moneyAmount): void
+    {
+        $this->moneyAmount = $moneyAmount;
+    }
+
+    /**
+     * @return Currency
+     */
+    public function getCurrency(): Currency
+    {
+        if (\is_object($this->currency)) {
+            return $this->currency;
+        }
+//        else {
+//            find currency in database, set currency and return
+//        }
+        // probably can replace get currency method in mapper
+    }
+
+    /**
+     * @param Currency $currency
+     */
+    public function setCurrency(Currency $currency): void
+    {
+        $this->currency = $currency;
+    }
 }
--- src/App/Model/WalletMapper.php
+++ src/App/Model/WalletMapper.php
@@ -18,4 +18,38 @@ class WalletMapper
     {
         $this->db = $db;
     }
+
+    /**
+     * @param int $userId
+     * @param string $currencyName
+     * @return Wallet
+     */
+    public function getWalletWithCurrency(int $userId, string $currencyName): ?Wallet
+    {
+        $sql = 'SELECT w.id AS wallet_id, w.money_amount '
+            . 'c.id AS currency_id, c.name AS currency_name, c.prec AS currency_precision'
+            . 'FROM wallet w '
+            . 'INNER JOIN users u ON w.user_id = u.id '
+            . 'INNER JOIN currency c ON w.currency_id = c.id '
+            . 'WHERE u.id = :id AND c.name = :currency_name;';
+        $params = ['id' => $userId, 'currency_name' => $currencyName];
+
+        if (!$walletDataArray = $this->db->query($sql, $params)->fetch()){
+            return null;
+        }
+
+        $wallet = new Wallet();
+
+        $wallet->setId($walletDataArray['id']);
+        $wallet->setMoneyAmount($walletDataArray['money_amount']);
+
+        $currency = new Currency();
+        $currency->setId($walletDataArray['currency_id']);
+        $currency->setName($walletDataArray['currency_name']);
+        $currency->setPrecision($walletDataArray['currency_precision']);
+
+        $wallet->setCurrency($currency);
+
+        return $wallet;
+    }
 }
